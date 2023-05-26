313a1f0d105b337699acf4b443607528c1c97ab2
kamil.palka <cabeq1236@gmail.com>
2023-05-26 19:21:28 +0200

    remove svg and prepare App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,31 @@
-<script setup>
-import HelloWorld from "./components/HelloWorld.vue";
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <ul>
+    <li></li>
+  </ul>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      storedResources: [
+        {
+          id: "place1",
+          title: "suuuper",
+          description: "guide",
+          link: "google.pl",
+        },
+        {
+          id: "place2",
+          title: "suuupe2",
+          description: "guide2",
+          link: "google.pl2",
+        },
+      ],
+    };
+  },
+};
+</script>
 
 <style scoped>
 .logo {
