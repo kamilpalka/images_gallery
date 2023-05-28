cd75194a22938c722a80a530daf0b275e3037ec9
kamil.palka <cabeq1236@gmail.com>
2023-05-29 00:53:17 +0200

    add vuetify to project

--- src/main.js
+++ src/main.js
@@ -3,6 +3,17 @@ import "./style.css";
 import App from "./App.vue";
 import store from "./store/index.js";
 
+// Vuetify
+import "vuetify/styles";
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+
+const vuetify = createVuetify({
+  components,
+  directives,
+});
+
 const app = createApp(App);
 
-app.use(store).mount("#app");
+app.use(store).use(vuetify).mount("#app");
