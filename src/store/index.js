fc15fbcb25980d411330baca4520ac0255ef37a7
kamil.palka <cabeq1236@gmail.com>
2023-05-29 00:28:26 +0200

    add store functionality

--- src/store/index.js
+++ src/store/index.js
@@ -3,8 +3,38 @@ import { createStore } from "vuex";
 const store = createStore({
   state() {
     return {
-      counter: 0,
+      images: [],
+      page: 1,
+      maxImages: 20,
     };
+  },
+  getters: {
+    sortImages: (state) =>
+      state.images.sort((a, b) => a.author.localeCompare(b.author)),
+    paginatedImages: (state, getters) => {
+      const start = (state.page - 1) * state.maxImages;
+      const end = start + state.maxImages;
+      return getters.sortImages.slice(start, end);
+    },
+  },
+  mutations: {
+    setImages(state, images) {
+      state.images = images;
+    },
+    setPage(state, page) {
+      state.page = page;
+    },
+  },
+  actions: {
+    async fetchImages({ context }) {
+      try {
+        const response = await fetch("https://picsum.photos/v2/list");
+        const images = await response.json();
+        context.commit("setImages", images);
+      } catch (error) {
+        console.log(error);
+      }
+    },
   },
 });
 
