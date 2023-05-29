61eda8197275eeabb1622de807cc715f8d3fe3df
kamil.palka <cabeq1236@gmail.com>
2023-05-29 22:07:12 +0200

    button style improvment

--- src/components/ImageList.vue
+++ src/components/ImageList.vue
@@ -1,45 +1,48 @@
 <template>
-  <div>
-    <v-app>
-      <v-main>
-        <v-container>
-          <v-row justify="center">
-            <v-col cols="12">
-              <h1 class="text-center">Image Gallery</h1>
-            </v-col>
-            <v-col
-              cols="12"
-              sm="6"
-              md="4"
-              lg="3"
-              v-for="image in sortImages"
-              :key="image.id"
-            >
-              <v-card class="mb-4" variant="tonal">
-                <v-img
-                  :src="image.download_url"
-                  :alt="image.author"
-                  height="200"
-                ></v-img>
-                <v-card-text class="text-center">{{
-                  image.author
-                }}</v-card-text>
-              </v-card>
-            </v-col>
-            <v-col cols="12" v-if="sortImages === 0">
-              <p class="text-center">No images found.</p>
-            </v-col>
-            <v-col cols="12" v-else>
-              <h2>{{ $store.state.page }}</h2>
-              <v-btn block color="primary" @click="fetchImages"
-                >Load More</v-btn
-              >
-            </v-col>
-          </v-row>
-        </v-container>
-      </v-main>
-    </v-app>
-  </div>
+  <v-app>
+    <v-main>
+      <v-container>
+        <v-row justify="center">
+          <v-col cols="12">
+            <h1 class="text-center">Image Gallery</h1>
+          </v-col>
+          <v-col
+            cols="12"
+            sm="6"
+            md="4"
+            lg="3"
+            v-for="image in sortImages"
+            :key="image.id"
+          >
+            <v-card class="mb-4" variant="tonal">
+              <v-img
+                :src="image.download_url"
+                :alt="image.author"
+                height="200"
+              ></v-img>
+              <v-card-text class="text-center">{{ image.author }}</v-card-text>
+            </v-card>
+          </v-col>
+          <v-col cols="12" v-if="sortImages === 0">
+            <p class="text-center">No images found.</p>
+          </v-col>
+        </v-row>
+        <v-row justify="center">
+          <v-btn
+            class="ma-3"
+            rounded="lg"
+            color="deep-orange-darken-3"
+            elevation="10"
+            variant="flat"
+            size="x-large"
+            max-width="300px"
+            @click="fetchImages"
+            >Load More
+          </v-btn>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
@@ -62,7 +65,7 @@ export default {
 
 <style>
 .v-main {
-  background-color: gray;
+  background-color: #ffccbc;
 }
 
 .v-card-text {
